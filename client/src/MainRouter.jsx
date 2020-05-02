2b908afd313e66798cc9d2a0d3dddda1f2261a27
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-05-02 01:08:47 -0700

    configured PriviateRoute.jsx

--- client/src/MainRouter.jsx
+++ client/src/MainRouter.jsx
@@ -3,8 +3,8 @@ import {Route, Switch} from "react-router-dom";
 import Index from './core/Home';
 import SignIn from "./User/SignIn";
 import SignUp from "./User/SignUp";
-import DashBoard from "./BTS/DashBoard";
 import Main from "./BTS/Main";
+import PrivateRoute from "./auth/PrivateRoute";
 
 const MainRouter = () => (
     <div>
@@ -13,13 +13,13 @@ const MainRouter = () => (
             <Route exact path={'/home'} component={Index}/>
             <Route exact path={'/signin'} component={SignIn}/>
             <Route exact path={'/signup'} component={SignUp}/>
-            <Route exact path={'/user/:userId/dashboard'} component={Main}/>
-            <Route exact path={'/user/:userId/products'} component={Main}/>
-            <Route exact path={'/user/:userId/products/:productId'} component={Main}/>
-            <Route exact path={'/user/:userId/history'} component={Main}/>
-            <Route exact path={'/user/:userId/messages'} component={Main}/>
-            <Route exact path={'/user/:userId/profile'} component={Main}/>
-            <Route exact path={'/user/:userId/setting'} component={Main}/>
+            <PrivateRoute exact path={'/user/:userId/dashboard'} component={Main}/>
+            <PrivateRoute exact path={'/user/:userId/products'} component={Main}/>
+            <PrivateRoute exact path={'/user/:userId/products/:productId'} component={Main}/>
+            <PrivateRoute exact path={'/user/:userId/history'} component={Main}/>
+            <PrivateRoute exact path={'/user/:userId/messages'} component={Main}/>
+            <PrivateRoute exact path={'/user/:userId/profile'} component={Main}/>
+            <PrivateRoute exact path={'/user/:userId/setting'} component={Main}/>
         </Switch>
     </div>
 );

f9a54ac8abaa201959dcd2bff707b1a14b31e71e
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-05-01 18:13:29 -0700

    update Link to message, profile, setting

--- client/src/core/SideNav/index.jsx
+++ client/src/core/SideNav/index.jsx
@@ -9,23 +9,23 @@ const SideNav = () =>(
         <div className="col col-2 bg-dark min-vh-100 nav-vertical">
             <div className="nav-tabs my-3">
                 <div className="row my-3"><Link to={`/user/dashboard/${isAuthenticated().user_id}`}
-                                             className="btn btn-dark mx-auto p-3 w-100 active">Dashboard</Link></div>
+                                             className="btn btn-dark mx-auto p-3 w-100">Dashboard</Link></div>
                 <div className="row my-3"><Link to={`/user/products/${isAuthenticated().user_id}`} className="btn btn-dark mx-auto p-3 w-100">My
                     Products</Link></div>
                 <div className="row my-3"><Link to={`/user/history/${isAuthenticated().user_id}`} className="btn btn-dark mx-auto p-3 w-100">Deal
                     History</Link></div>
                 <div className="row my-3">
-                    <button className="btn btn-dark mx-auto p-3 w-100">Messages</button>
+                        <Link to={`/user/messages/${isAuthenticated().user_id}`} className="btn btn-dark mx-auto p-3 w-100">Messages</Link>
                 </div>
                 <div className="row my-3">
-                    <button className="btn btn-dark mx-auto p-3 w-100">Profile</button>
+                        <Link to={`/user/profile/${isAuthenticated().user_id}`} className="btn btn-dark mx-auto p-3 w-100">Profile</Link>
                 </div>
                 <div className="row my-3">
-                    <button className="btn btn-dark mx-auto p-3 w-100">Setting</button>
+                        <Link to={`/user/setting/${isAuthenticated().user_id}`} className="btn btn-dark mx-auto p-3 w-100">Setting</Link>
                 </div>
             </div>
         </div>
     </>
-);
+)
 
 export default SideNav;
